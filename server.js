5529d90023c52acf711dc28c12a3aa23c0bf860f
moosius1 <imoe61@gmail.com>
2024-02-10 12:19:45 -0700

    attempted fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 
 const mongodb = require('./dbConnect');
-const { createNew } = require('./controllers/projects');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -15,12 +14,7 @@ app
 .use(express.json())
 .use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  );
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
   next();
 })
 .use('/', require('./routes'));
--- swagger.js
+++ swagger.js
@@ -2,8 +2,8 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'Project Manager API',
-    description: 'CRUD Project Manager ',
+    title: 'My API',
+    description: 'Temple API',
   },
   host: 'cse-341-w05-w08.onrender.com',
   schemes: ['https'],
@@ -15,7 +15,8 @@ const endpointsFiles = ['./routes/index.js'];
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
-
+// Run server after it gets generated
 // swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
 //   await import('./index.js');
 // });
+
