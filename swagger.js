e20608e90f571cc6a961cb1343607b752a1a0ce9
moosius1 <imoe61@gmail.com>
2024-03-01 22:17:27 -0700

    render test working with api-docs

--- swagger.js
+++ swagger.js
@@ -1,9 +1,13 @@
+
+
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
+  swagger: '2.0', // Set the Swagger version to 2.0
   info: {
     title: 'My API',
-    description: 'Temple API',
+    version: '1.0.0', // You can specify your own version here
+    description: 'Project Management API',
   },
   host: 'cse-341-w05-w08.onrender.com',
   schemes: ['https'],
@@ -12,10 +16,6 @@ const doc = {
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// generate swagger.json
-// swaggerAutogen(outputFile, endpointsFiles, doc);
-
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./server.js');
-// });
+swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+  await import('./server.js');
+});
