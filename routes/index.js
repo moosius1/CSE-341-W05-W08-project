8b8448132476df48e360cb6027cfb6e9e68af358
moosius1 <imoe61@gmail.com>
2024-03-02 22:06:46 -0700

    fixed /contributors addition in index.js router

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/projects', require('./projects'));
-router.use('contributors', require('./contributors'));
+router.use('/contributors', require('./contributors'));
 
 module.exports = router;
